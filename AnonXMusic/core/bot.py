efb39ed4e8d019082f533c3864753d394cc8f02f
IKEX01 <ccchekingusa@gmail.com>
2025-05-25 17:06:59 +0530

    Update bot.py

--- AnonXMusic/core/bot.py
+++ AnonXMusic/core/bot.py
@@ -7,7 +7,7 @@ from ..logging import LOGGER
 
 class Anony(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Sᴛᴀʀᴛɪɴɢ Sᴛᴏʀᴍ Mᴜsɪᴄ Bᴀʙʏ...")
+        LOGGER(__name__).info(f"Sᴛᴀʀᴛɪɴɢ Bᴀʙʏ...")
         super().__init__(
             name="Opus",
             api_id=config.API_ID,
