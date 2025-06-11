ab35338b27d367f627b4046c05fa6ec027f9a403
SpaceXe-tech <billaxe299@gmail.com>
2025-06-12 00:18:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 9999999))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
-# Get this value  on Telegram by /id
+# Get this value on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 5960968099))
 
 ## Fill these variables if you're deploying on heroku.
@@ -45,21 +45,17 @@ SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/storm_core")
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
-
 # Get this credentials from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "95f4f5c6df5744698035a0948e801ad9")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "4b03167b38c943c3857333b3f5ea95ea")
 
-
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 500))
-
 
 # Telegram audio and video file size limit (in bytes)
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
-
 
 # Get your pyrogram v2 session from @StringFatherBot on Telegram
 STRING1 = getenv("STRING_SESSION", None)
@@ -68,14 +64,12 @@ STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
 
-
 BANNED_USERS = filters.user()
 adminlist = {}
 lyrical = {}
 votemode = {}
 autoclean = []
 confirmer = {}
-
 
 START_IMG_URL = getenv(
     "START_IMG_URL", "https://graph.org/file/6b54d8b294fe0b4934713-bb809aee23c5f19aa9.jpg"
@@ -94,14 +88,15 @@ SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/4491c4c570d9ccb1a19d0-1fb0835d3
 SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/4491c4c570d9ccb1a19d0-1fb0835d3592780331.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/4491c4c570d9ccb1a19d0-1fb0835d3592780331.jpg"
 
+# Add API_URL1 and API_URL2 loaded from .env
+API_URL1 = getenv("API_URL1", None)
+API_URL2 = getenv("API_URL2", None)
 
 def time_to_seconds(time):
     stringt = str(time)
-    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
-
+    return sum(int(x * 60**i for i, x in enumerate(reversed(stringt.split(":")))))
 
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
-
 
 if SUPPORT_CHANNEL:
     if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
