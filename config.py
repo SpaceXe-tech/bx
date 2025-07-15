0eb1c5d248bae057c5046072c6b36084abe654bf
SpaceXe-tech <billaxe299@gmail.com>
2025-07-15 17:05:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,6 +30,8 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+COOKIES = getenv("COOKIES", None)
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/BillaSpace/RX7",
