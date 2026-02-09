394b0a43b4a7b9cb668e69adde49d88038130af0
SpaceXe-tech <billashukla6@gmail.com>
2026-02-10 02:57:04 +0530

    Update __main__.py

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -12,8 +12,6 @@ from AnonXMusic.plugins import ALL_MODULES
 from AnonXMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
-# Import the restart task
-from AnonXMusic.plugins.sudo.autoboot import auto_restart, auto_restart_task
 
 async def init():
     if (
@@ -23,48 +21,51 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        LOGGER(__name__).error("Assistant client variables not defined. Exiting.")
         exit()
+
     await sudo()
+
     try:
         users = await get_gbanned()
         for user_id in users:
             BANNED_USERS.add(user_id)
+
         users = await get_banned_users()
         for user_id in users:
             BANNED_USERS.add(user_id)
     except:
         pass
+
     await app.start()
-    for all_module in ALL_MODULES:
-        importlib.import_module("AnonXMusic.plugins" + all_module)
-    LOGGER("AnonXMusic.plugins").info("Successfully Imported Modules...")
+
+    for module in ALL_MODULES:
+        importlib.import_module("AnonXMusic.plugins" + module)
+
+    LOGGER("AnonXMusic.plugins").info("Modules imported successfully.")
+
     await userbot.start()
     await Anony.start()
+
     try:
         await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("AnonXMusic").error(
-            "Please turn on the videochat of your log group/channel.\n\nStopping Bot..."
+            "Start a video chat in your log group or channel. Stopping bot."
         )
         exit()
     except:
         pass
-    await Anony.decorators()
-    LOGGER("AnonXMusic").info(
-        "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
-    )
 
-    # ✅ Start default 2-hour restart only if no manual task is running
-    global auto_restart_task
-    if not auto_restart_task or auto_restart_task.done():
-        auto_restart_task = asyncio.create_task(auto_restart(1440))
-        LOGGER("AutoBoot").info("✅ Default auto-restart scheduled for every 12 hours.")
+    await Anony.decorators()
+
+    LOGGER("AnonXMusic").info("AnonX Music Bot Started Successfully.")
 
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("AnonXMusic").info("Stopping Billa Music Bot...")
+    LOGGER("AnonXMusic").info("Bot Stopped.")
+
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
